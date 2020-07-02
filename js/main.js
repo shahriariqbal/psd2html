428d046d8f24361be4f04c387a57896e53972b97
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-07-02 21:57:26 +0600

    iPhone Animation

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready ( function() {
+
+    $('.js-wp-2').waypoint( function(direction) {
+
+        $('.js-wp-2').addClass('animated   animate__animated animate__slideInUp');
+
+    }, {
+        offset: '70%'
+    });
+
+});
+
+
+
